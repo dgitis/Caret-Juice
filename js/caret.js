2e020d1654deb50eb1096d680c94d9e506ab0d51
Damon Gudaitis <damon.gudaitis@gmail.com>
2015-10-08 15:36:07 -0700

    Removed unused JS

--- js/caret.js
+++ js/caret.js
@@ -1,7 +1,6 @@
 // @codekit-prepend "../bower_components/fontfaceobserver/fontfaceobserver.js";
 // @codekit-prepend "../bower_components/foundation/js/foundation.js";
 // @codekit-prepend "../bower_components/foundation/js/foundation/foundation.abide.js";
-// @codekit-prepend "../bower_components/foundation/js/foundation/foundation.clearing.js";
 // @codekit-prepend "../bower_components/foundation/js/foundation/foundation.offcanvas.js";
 // @codekit-prepend "../bower_components/foundation/js/foundation/foundation.topbar.js";
 // @codekit-prepend "app.js";
