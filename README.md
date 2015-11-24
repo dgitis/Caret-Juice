d7ef6a22def4dc93263e84b582c9ac79f41048a5
Damon Gudaitis <damon.gudaitis@gmail.com>
2015-11-24 14:38:35 -0800

    Switch to Middleman
    Switched static site generators from CodeKit to Middleman.

--- README.md
+++
@@ -1,3 +0,0 @@
-# Web Files for Caret Juice Marketing
-
-Nothing to see here. Just a simple website.
--- js/caret.js
+++
@@ -1,8 +0,0 @@
-// @codekit-prepend "../bower_components/fontfaceobserver/fontfaceobserver.js";
-// @codekit-prepend "../bower_components/foundation/js/foundation.js";
-// @codekit-prepend "../bower_components/foundation/js/foundation/foundation.abide.js";
-// @codekit-prepend "../bower_components/foundation/js/foundation/foundation.offcanvas.js";
-// @codekit-prepend "../bower_components/foundation/js/foundation/foundation.topbar.js";
-// @codekit-prepend "app.js";
-// @codekit-prepend "lazyYT.js";
-// @codekit-prepend "alt-tab.js";
