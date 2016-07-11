729ff007cbe828e3fba5f0eb4deeaf367a94cf64
damon <damon.gudaitis@gmail.com>
2016-07-11 14:40:47 -0700

    Revert Directory Change

--- source/javascripts/signup-bar.js
+++
@@ -1,9 +0,0 @@
-(function ($, root, undefined) { 
-	$(function () { 
-		'use strict'; 
-		// DOM ready, take it away 
-		$('.signup-bar .close').on('click',function(){ 
-			$(this).parent().fadeOut('slow'); 
-		}); 
-	}); 
-})(jQuery, this);
