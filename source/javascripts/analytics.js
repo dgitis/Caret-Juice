fdc96d56a290104c77fd9b54f55244b8e35b9ff4
dgitis <damon@caretjuice.com>
2018-10-29 13:51:34 -0700

    Comments. Lot's of comments.

--- source/javascripts/analytics.js
+++ source/javascripts/analytics.js
@@ -1,13 +1,33 @@
 /*
-ga(function(tracker) {
-    var clientId = tracker.get('clientId');
-    console.log(clientId);
-  });
+Get the clientID from Google Analytics and insert it into hidden form field.
+The submitted value goes in to marketing automation platform to later reference when 
+submitting online transactions.
+
+On pages with multiple forms, this should insert the clientID into all forms.
+
+There may be multiple GA tracker objects on the page. We should be able to 
+configure which tracker object we want to get the client ID from.
+
+Docs:
+JS-Cookie https://github.com/js-cookie/js-cookie
+GA Tracker https://developers.google.com/analytics/devguides/collection/analyticsjs/cookies-user-id
 */
-ga(function() {
-    var t = ga.getAll(), clientId;
-    if(t && t.length) {
-    clientId = t[0].get('clientId');
-    document.getElementById('clid').value = clientId;
-    }
+$(document).ready(function(){ // Shouldn't need this with GA Ready Callback
+    ga(function(tracker) {  //GA Ready Callback (not working) https://developers.google.com/analytics/devguides/collection/analyticsjs/command-queue-reference#ready-callback
+        var clientId = tracker.get('clientId');
+        document.prop('#clid').value = clientId;//may need to use Hit Callback https://developers.google.com/analytics/devguides/collection/analyticsjs/sending-hits#hitcallback
+        console.log(clientId);
+  });
 });
+/*
+$(document).ready(function(){
+    ga(function() {
+        var t = ga.getAll(), clientId;
+        if(t && t.length) {
+            clientId = t[0].get('clientId');
+            document.getElementById('clid').value = clientId;
+            console.log(clientId);
+        }
+    });
+});
+*/
--- source/javascripts/cookie-utils.js
+++ source/javascripts/cookie-utils.js
@@ -1,28 +1,30 @@
-//suppress optout bar for people with ga_opt_out cookie
+/*
+If you have the set your cookie preferences, hide the privacy nag.
+*/
 function optOutPrefSet(){
     var cookie = Cookies.get("ga_opt_out");
-    console.log("cookie var= " + cookie);
-    if (typeof cookie == "undefined" ){
-        //suppress optOutBar
+    console.log("cookie var:" + cookie);
+    if (typeof cookie != "undefined" ){  //suppress optOutBar
         var optOutBar = document.getElementById('signup-bar');
         optOutBar.style.display = 'none';
     }
 }
 
-//toggle the cookie value
+//toggle the cookie value at privacy.html
 function toggleTracking() {
     var status = Cookies.get("ga_opt_out");
    if (typeof status == "undefined" ) {
        cookieValue = true; //first click should turn off tracking
    }
    else {
-       cookieValue = !cookieValue;
+       cookieValue = $('#toggleOptOUt').prop("checked"); 
+       cookieValue = !cookieValue; //toggle true/false
    }
    Cookies.set("ga_opt_out", cookieValue, { expires: 3650 }, { domain: 'www.caretjuice.com' });
    console.log("attempted to set cookie");
 }
 
-//accept Tracking
+//dismissing the privacy nag means you accept tracking
 function acceptTracking() {
    Cookies.set("ga_opt_out", "false", { expires: 3650 }, { domain: 'www.caretjuice.com' });
 }
@@ -31,23 +33,14 @@ function acceptTracking() {
 $(document).ready(function () {
     optOutPrefSet(); //while we are here, let's also see if we need to display the Opt Out Bar
 	 s = Cookies.get("ga_opt_out");
-	 if (s == true ){
+	 if (s == true ){  // this line isn't matching (cookieVar and s are equal in console)
          $('#toggleOptOUt').prop('checked', true);
          console.log("s value:" + s);
          console.log("Checked opt out status cookie, found it and set toggle to checked");
 	 }
 	 else {
          $('#toggleOptOUt').prop('checked', false);
+         console.log("s value:" + s);
          console.log("Failed to find opt out status cookie and set toggle to unchecked")
 	 }
-});
-
-
-//QA Code to be removed
-$(document).ready(function(){
-	Cookies.set('jscookie', 'TEST', { expires: 2 });
-	if (Cookies.get('jscookie')) {
-		$('body').append(Cookies.get('jscookie'));
-		Cookies.remove('jscookie');
-	}
 });
